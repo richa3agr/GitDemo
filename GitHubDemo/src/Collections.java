b037f52f2ad8c782548ed8dc6ecd8b2cb6a7d0d6
I367127 <I367127@DSM2CJ4K.adhcscint.net>
2017-06-27 09:08:31 -0500

    First commit

---
+++ GitHubDemo/src/Collections.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		arraylist();
+		
+
+	}
+
+	private static void arraylist() {
+		ArrayList<String> name= new ArrayList<String>();
+		
+		name.add(0, "Richa");
+		
+		name.add(1, "Sara");
+		
+		Iterator<String> itr= name.iterator();
+		
+		while(itr.hasNext())
+		{
+		System.out.println(itr);
+		}
+		
+		
+	}
+
+}
